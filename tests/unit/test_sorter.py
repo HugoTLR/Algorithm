1764c7d5d9af23ca2b37ee7e35d3e4f0cd130e7b
vV <hugoruellet@gmail.com>
2020-10-09 18:59:35 +0200

    Best practices for python unittest

--- tests/unit/test_sorter.py
+++ tests/unit/test_sorter.py
@@ -2,48 +2,56 @@ import unittest
 
 from Classes import sorter
 
-class BaseTestCases:
+class TestSorter(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    if cls is not TestSorter and cls.setUp is not TestSorter.setUp:
+      orig_setUp = cls.setUp
+      def setUpOverride(self, *args, **kwargs):
+        TestSorter.setUp(self)
+        return orig_setUp(self, *args, **kwargs)
+      cls.setUp = setUpOverride
 
-  class TestSorter(unittest.TestCase):
-
-    def setUp(self):
-      self.data = [0,9,8,6,3,5,4,2,1,7]
-      self.data_size = len(self.data)
-
-    def test_swap(self):
-      i, j = 1, 8
-      self.assertTrue(self.data_size > 0)
-      self.assertTrue( (i >= 0 and i < self.data_size) )
-      self.assertTrue( (j >= 0 and j < self.data_size and j != i ))
-
-    # def test_build_status(self):
-    #   raise NotImplementedError('Use child function')
-
-
-class TestInsertion(BaseTestCases.TestSorter):
   def setUp(self):
-    super(TestInsertion, self).setUp()
-    self.sorter = sorter.Insertion()
+    self.data = [0,9,8,6,3,5,4,2,1,7]
+    self.data_size = len(self.data)
 
-  def test_insertion_swap(self):
-    super(TestInsertion, self).test_swap()
+  def test_swap(self):
+    i, j = 1, 8
+    self.assertTrue(self.data_size > 0)
+    self.assertTrue( (i >= 0 and i < self.data_size) )
+    self.assertTrue( (j >= 0 and j < self.data_size and j != i ))
+    result = sorter.Sorter().swap(self.data,i,j)
+    self.assertEqual(result, [0,1,8,6,3,5,4,2,9,7] )
+
+class TestInsertion(TestSorter):
+  def setUp(self):
+    self.sorter = sorter.Insertion()
+    self.sorter.data = self.data
+    self.sorter.data_size = self.data_size
 
   def test_insertion_build_status(self):
     """
     Test that we are correctly building insertion sort status
     """
     idx = 3
-    result = self.sorter.build_status(idx,self.data_size)
+    result = self.sorter.build_status(idx)
     self.assertEqual(result, [1,1,1,1,2,0,0,0,0,0])
 
+  def test_insertion_sort(self):
+    """
+    Test that we are correctly sorting data
+    """
+    self.sorter.sort(self.data)
+    result = self.sorter.steps[-1]
+    self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
 
-class TestSelection(BaseTestCases.TestSorter):
+
+class TestSelection(TestSorter):
   def setUp(self):
-    super(TestSelection, self).setUp()
     self.sorter = sorter.Selection()
-
-  def test_selection_swap(self):
-    super(TestSelection, self).test_swap()
+    self.sorter.data = self.data
+    self.sorter.data_size = self.data_size
 
   def test_selection_build_status(self):
     """
@@ -51,139 +59,159 @@ class TestSelection(BaseTestCases.TestSorter):
     """
     idx = 3
     idx2 = 5
-    result = self.sorter.build_status(idx,idx2,self.data_size)
-    self.assertEqual(result, [1,1,1,1,0,2,0,0,0,0])
+    result = self.sorter.build_status(idx,idx2)
+    self.assertEqual(result, [1,1,1,2,0,3,0,0,0,0])
+
+  def test_selection_sort(self):
+    """
+    Test that we are correctly sorting data
+    """
+    self.sorter.sort(self.data)
+    result = self.sorter.steps[-1]
+    self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
 
 
-class TestQuicksort(BaseTestCases.TestSorter):
+class TestQuicksort(TestSorter):
   def setUp(self):
-    super(TestQuicksort, self).setUp()
     self.sorter = sorter.Quicksort()
-
-  def test_quicksort_swap(self):
-    super(TestQuicksort, self).test_swap()
+    self.sorter.data = self.data
+    self.sorter.data_size = self.data_size
 
   def test_quicksort_build_status(self):
     """
     Test that we are correctly building quicksort status
     """
-    result = self.sorter.build_status(self.data_size, self.data_size - 1)
-    self.assertEqual(result, [0,0,0,0,0,0,0,0,0,2])
+    low, high, i = 3, 8, 5
+    result = self.sorter.build_status(i, low, high)
+    self.assertEqual(result, [1,1,1,4,0,2,0,0,3,0])
 
-    i = 5
-    low = 4
-    high = 6
-    result = self.sorter.build_status(self.data_size, i, low, high)
-    self.assertEqual(result, [1,1,1,1,3,2,3,0,0,0])
+  def test_quicksort_sort(self):
+    """
+    Test that we are correctly sorting data
+    """
+    self.sorter.sort(self.data)
+    result = self.sorter.steps[-1]
+    self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
 
-class TestBubblesort(BaseTestCases.TestSorter):
+
+class TestBubblesort(TestSorter):
   def setUp(self):
-    super(TestBubblesort, self).setUp()
     self.sorter = sorter.Bubblesort()
-
-  def test_bubblesort_swap(self):
-    super(TestBubblesort, self).test_swap()
+    self.sorter.data = self.data
+    self.sorter.data_size = self.data_size
 
   def test_bubblesort_build_status(self):
     """
     Test that we are correctly building bubblesort status
     """
-    result = self.sorter.build_status(self.data_size)
-    self.assertEqual(result, [0,0,0,0,0,0,0,0,0,0])
-    
     idx = 2
     n = self.data_size - 1 
-    result = self.sorter.build_status(self.data_size,idx - 1, idx, n)
+    result = self.sorter.build_status(idx - 1, idx, n)
     self.assertEqual(result, [0,2,2,0,0,0,0,0,0,1])
 
+  def test_bubblesort_sort(self):
+    """
+    Test that we are correctly sorting data
+    """
+    self.sorter.sort(self.data)
+    result = self.sorter.steps[-1]
+    self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
 
 
 
-class TestBubblesortOptimized(BaseTestCases.TestSorter):
+class TestBubblesortOptimized(TestSorter):
   def setUp(self):
-    super(TestBubblesortOptimized, self).setUp()
+    self.sorter = sorter.Bubblesort_Optimized()
+    self.sorter.data = self.data
+    self.sorter.data_size = self.data_size
 
-  def test_swap(self):
-    super(TestBubblesortOptimized, self).test_swap()
+  def test_bubblesort_optimized_build_status(self):
+    """
+    Test that we are correctly building bubblesort status
+    """
+    idx = 2
+    n = self.data_size - 1 
+    result = self.sorter.build_status(idx - 1, idx, n)
+    self.assertEqual(result, [0,2,2,0,0,0,0,0,0,1])
 
-class TestBubblesortOptimized2(BaseTestCases.TestSorter):
+  def test_bubblesort_optimized_sort(self):
+    """
+    Test that we are correctly sorting data
+    """
+    self.sorter.sort(self.data)
+    result = self.sorter.steps[-1]
+    self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
+
+
+class TestBubblesortOptimized2(TestSorter):
   def setUp(self):
-    super(TestBubblesortOptimized2, self).setUp()
+    self.sorter = sorter.Bubblesort_Optimized_2()
+    self.sorter.data = self.data
+    self.sorter.data_size = self.data_size
 
-  def test_swap(self):
-    super(TestBubblesortOptimized2, self).test_swap()
+  def test_bubblesort_optimized_2_build_status(self):
+    """
+    Test that we are correctly building bubblesort status
+    """
+    idx = 2
+    n = self.data_size - 1 
+    result = self.sorter.build_status(idx - 1, idx, n)
+    self.assertEqual(result, [0,2,2,0,0,0,0,0,0,1])
 
-class TestGnomesort(BaseTestCases.TestSorter):
+  def test_bubblesort_optimized_2_sort(self):
+    """
+    Test that we are correctly sorting data
+    """
+    self.sorter.sort(self.data)
+    result = self.sorter.steps[-1]
+    self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
+
+
+class TestCombsort(TestSorter):
   def setUp(self):
-    super(TestGnomesort, self).setUp()
+    self.sorter = sorter.Combsort()
+    self.sorter.data = self.data
+    self.sorter.data_size = self.data_size
 
-  def test_swap(self):
-    super(TestGnomesort, self).test_swap()
+  def test_combsort_build_status(self):
+    """
+    Test that we are correctly building bubblesort status
+    """
+    gap = 3
+    idx = 5
+    result = self.sorter.build_status(gap,idx,gap+idx)
+    self.assertEqual(result, [0,0,0,0,0,4,2,2,3,0])
 
-
-
-# class TestInsertion(unittest.TestCase):
-
-#   def test_sort(self):
-#     """
-#     Test that we are correctly sorting data
-#     """
-#     data = [0,9,8,6,3,5,4,2,1,7]
-#     self.sorter.sort(data)
-#     result = self.sorter.steps[-1]
-#     self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
-
-
-# class TestSelection(unittest.TestCase):
-
-#   def test_sort(self):
-#     """
-#     Test that we are correctly sorting data
-#     """
-#     data = [0,9,8,6,3,5,4,2,1,7]
-#     self.sorter.sort(data)
-#     result = self.sorter.final_step
-#     self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
+  def test_combsort_sort(self):
+    """
+    Test that we are correctly sorting data
+    """
+    self.sorter.sort(self.data)
+    result = self.sorter.steps[-1]
+    self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
 
 
-# class TestQuicksort(unittest.TestCase):
-#   def setUp(self):
-#     self.sorter = sorter.Quicksort()
+class TestGnomesort(TestSorter):
+  def setUp(self):
+    self.sorter = sorter.Gnomesort()
+    self.sorter.data = self.data
+    self.sorter.data_size = self.data_size
 
-#   def test_build_status(self):
-#     """
-#     Test that we are correctly building quicksort status
-#     """
-#     data = [1,0,8,4,6,7,5,3,6,9]
-#     data_size = len(data)
-#     result = self.sorter.build_status(data_size, data_size - 1)
-#     self.assertEqual(result, [0,0,0,0,0,0,0,0,0,2])
+  def test_gnomesort_build_status(self):
+    """
+    Test that we are correctly building bubblesort status
+    """
+    idx = 5
+    result = self.sorter.build_status(idx)
+    self.assertEqual(result, [1,1,1,1,1,2,0,0,0,0])
 
-#     i = 5
-#     low = 4
-#     high = 6
-#     result = self.sorter.build_status(data_size, i, low, high)
-#     self.assertEqual(result, [1,1,1,1,3,2,3,0,0,0])
-
-#   def test_sort(self):
-#     """
-#     Test that we are correctly sorting data
-#     """
-#     data = [0,9,8,6,3,5,4,2,1,7]
-#     self.sorter.sort(data)
-#     result = self.sorter.final_step
-#     self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
-
-#   def test_partition(self):
-#     """
-#     Test that we are correctly partitining data
-#     """
-#     data = [0,9,8,6,3,5,4,2,1,7]
-#     low, high = 2, 5
-#     result = self.sorter.partition(data, low, high)
-#     self.assertEqual(result, 3)
-
-
+  def test_gnomesort_sort(self):
+    """
+    Test that we are correctly sorting data
+    """
+    self.sorter.sort(self.data)
+    result = self.sorter.steps[-1]
+    self.assertEqual(result, [0,1,2,3,4,5,6,7,8,9])
 
 if __name__ == "__main__":
   unittest.main()
